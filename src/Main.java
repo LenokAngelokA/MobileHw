934cbc0c6b014686ef84f75c1c04dd4e7c2cf218
Elena <arhangelskaya.lena@mail.ru>
2024-04-06 17:12:48 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int startingScore = 200;
+        int replenishment = 700;
+
+        if (replenishment > 500) {
+            int endbonus = replenishment / 100;
+
+
+            int totalAmount = startingScore + endbonus + replenishment;
+
+
+            System.out.println("Итоговая сумма на счету" + totalAmount);
+            System.out.println("Количичество бонусов" + endbonus); }
+        if (replenishment < 500 ) {
+            int totalAmount = startingScore + replenishment;
+
+                System.out.println("Бонусов нет" );
+                System.out.println("Итоговая сумма на счету" + totalAmount);
+            }
+
+    }
+
+}
+
